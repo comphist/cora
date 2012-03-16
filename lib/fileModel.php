c899590578fd86e78535fed75b16e56a7afce204
Lara Kresse <lara.kresse@rub.de>
2012-03-16 11:39:43 +0000

    comments added

--- lib/fileModel.php
+++ lib/fileModel.php
@@ -1,11 +1,30 @@
 <?php
 
+/** @file contentModel.php
+ * Define classes related to file import.
+ *
+ * @author Lara Kresse
+ * @date February 2012
+ */
+
+/** This class provides methods for importing new files and adding tagged data
+ * to existing ones.
+ *
+ */
 class fileModel{
 	
+	/** Create a new fileModel with the reference to the @c sessionHandler. */
 	public function __construct(&$sh){
 		$this->sh = $sh;
 	}
 	
+	/** Check if a tag exists in the given tagset.
+	 *
+	 * @note Attention: at the moment NO TAGSET ALIGNMENT is done
+	 * @todo enable tagset alignment
+	 * @param string $tag the tag
+	 * @return bool result of the @c in_array function
+	 */
 	function prooveTag($tag){
 		if(in_array($tag,$this->tagsetTags)){
 			return true;
@@ -13,13 +32,28 @@ class fileModel{
 		return false;
 	}
 	
-	function prooveAttrib(){
-		if(in_array($tag,$this->tagsetAttribs)){
+	/** Check if a attribute exists in the given tagset.
+	 *
+	 * @note Attention: at the moment NO TAGSET ALIGNMENT is done
+ 	 * @todo enable tagset alignment and implement the attribute look up
+	 * @param string $attrib the tag
+	 * @return bool result of the @c in_array function
+	 */
+	function prooveAttrib($attrib){
+		if(in_array($attrib,$this->tagsetAttribs)){
 			return true;
 		}
 		return false;		
 	}
 	
+	/** Initialize the tagset.
+	 *
+	 * The tagset is retrieved from the database und than
+	 * stored in two class arrays distinguished by type (tag or attribute).
+	 *
+	 * @note Attention: at the moment NO TAGSET ALIGNMENT is done (no @c preprocessTagset() is needed)
+	 * @param string $tagset the tagset's name
+	 */	
 	function preprocessTagset($tagset){
 		$dbTagset = $this->sh->getTagset($tagset);
 		
@@ -37,8 +71,24 @@ class fileModel{
 		$this->tagsetAttribs = $tagsetAttribs;
 	}
 	
+	/** Import an new file.
+	 *
+	 * Wrapper for @c processLine funtion.
+	 * Uploaded data is splitted by lines.
+	 *
+	 * @param string $tagset the tagset's name used for the text
+	 * @param bool $pos tag type of the text: POS (@true or @false), default: @c false
+	 * @param bool $morph tag type of the text: Morph (@true or @false), default: @c false
+	 * @param bool $norm tag type of the text: Norm (@true or @false), default: @c false
+	 * @param string reference $data uploaded tag data (from @c file_get_contents())	
+	 *
+	 * @return an @em array with at least a @c status key which indicates the success of the import
+	 * and a @c data key which contains either the imported data or the errors to pass them back to
+	 * the user
+	 */		
 	public function importFile($tagset,$pos=false,$morph=false,$norm=false,&$data){
-
+        
+		/* TAGSET ALIGNEMENT IS DISABLED */
 		// if($pos || $morph)
 		// 	$this->preprocessTagset($tagset);
 			
@@ -57,7 +107,23 @@ class fileModel{
 		return array("status"=>true, "data"=>$this->dbData);
 		
 	}
-			
+
+	/** Get structured tag data from a text line.
+	 *
+	 * Lines are splitting by tab stops and resulting parts are wrote to an associative array, keys are 
+	 * depending on the tag type (POS, Morph or Norm). The resulting array is appended to the class array
+	 * @c dbData.
+	 * Following formats are assumed:
+	 * POS: token \t pos_tag \t lemma \t possibility
+ 	 * Morph: token \t morph_tag \t lemma \t possibility
+	 * Norm: token \t normalised form \t possibility
+	 *   
+	 * @param string $line the text line
+	 * @param bool $pos tag type of the text: POS (@true or @false)
+	 * @param bool $morph tag type of the text: Morph (@true or @false)
+	 * @param bool $norm tag type of the text: Norm (@true or @false)
+	 *
+	 */					
 	public function processLine($line,$pos,$morph,$norm){
 		
 		if(empty($line)) return;
@@ -73,6 +139,7 @@ class fileModel{
 					$splits = explode(" ",$part);
 					if($pos){
 						$tmp['pos'] = trim($splits[0]);
+						/* TAGSET ALIGNEMENT IS DISABLED */
 						// if(!$this->prooveTag($tmp['pos'])){
 						// 	$this->errors[$tmp['pos']] = $partId;
 						// 	continue;														      
@@ -81,6 +148,7 @@ class fileModel{
 						$tmp['possibility'] = trim($splits[2]);
 					} elseif($morph){
 						$tmp['morph'] = trim($splits[0]);
+						/* TAGSET ALIGNEMENT IS DISABLED */
 						// if(!$this->prooveAttrib($tmp['morph'])){
 						// 	$this->errors[$tmp['morph']] = $partId;
 						// 	continue;														      
@@ -103,8 +171,30 @@ class fileModel{
 		}
 	}
 		
-	
+	/** Add data to an existing file.
+	 *
+	 * Wrapper for @c processLine funtion.
+	 * Uploaded data is splitted by lines.
+	 * 
+	 * Before passing data to the @ processLine method, a token check is performed: the
+	 * token is compared to the token of the respective line in the database. In case of
+	 * dismatch, the line is skipped and a @c diff counter stores the difference so the
+	 * following lines are matched to the correct lines (new line id = old line id +- diff)!
+	 * 
+	 * @param int $fileid file ID where to add the tag data
+	 * @param string $tagset the tagset's name used for the text
+	 * @param bool $pos tag type of the text: POS (@true or @false), default: @c false
+	 * @param bool $morph tag type of the text: Morph (@true or @false), default: @c false
+	 * @param bool $norm tag type of the text: Norm (@true or @false), default: @c false
+	 * @param string reference $data uploaded tag data (from @c file_get_contents())
+	 *
+	 * @return an @em array with at least a @c status key which indicates the success
+	 * and a @c data key which contains either the added data or the errors to pass them back to
+	 * the user
+	 */
 	public function addData($fileid,$tagset,$pos=false,$morph=false,$norm=false,&$data){
+		
+		/* TAGSET ALIGNEMENT IS DISABLED */
 		// if($pos || $morph)
 		// 	$this->preprocessTagset($tagset);
 
