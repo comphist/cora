d5dd3dc9171b73eab1956a2f6d2d780ef307bf12
Marcel Bollmann <bollmann@linguistics.rub.de>
2013-12-11 14:49:37 +0000

    Yet another bugfix for yet another bugfix for previous.

--- lib/sessionHandler.php
+++ lib/sessionHandler.php
@@ -269,7 +269,7 @@ class CoraSessionHandler {
     // HACK: project-specific hack hidden here!
     $cmdopt = null;
     foreach($options['tagsets'] as $tsid) {
-      if(array_key_exists($TRANS_IMPORT_AUTOTAG_OPTIONS, $tsid)) {
+      if(array_key_exists($tsid, $TRANS_IMPORT_AUTOTAG_OPTIONS)) {
 	$cmdopt = $TRANS_IMPORT_AUTOTAG_OPTIONS[$tsid];
       }
     }
