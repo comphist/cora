f412ec428c14bc714155ff4ec9d20682b7175bfe
Marcel Bollmann <bollmann@linguistics.rub.de>
2015-04-20 13:24:02 +0000

    Simple debug script.

---
+++ bin/debug_memory_usage.php
@@ -0,0 +1,19 @@
+<?php
+$CORA_DIR = dirname(__FILE__) . "/../";
+$DEVNULL = fopen("/dev/null", "a");
+require_once( $CORA_DIR . "lib/globals.php" );
+require_once( $CORA_DIR . "lib/connect.php" );
+require_once( $CORA_DIR . "lib/exporter.php" );
+$dbi = new DBInterface(DB_SERVER, DB_USER, DB_PASSWORD, MAIN_DB);
+$exp = new Exporter($dbi);
+
+memprof_enable();
+//$data = $dbi->openFile(149);
+//$exp->export(149, ExportType::CoraXML, array(), $DEVNULL);
+$data = $dbi->getAllModerns(149);
+memprof_dump_pprof(STDOUT);
+
+fwrite(STDERR, "Memory usage:      ".memory_get_usage()."\n");
+fwrite(STDERR, "Memory peak usage: ".memory_get_peak_usage()."\n");
+
+?>
