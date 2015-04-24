207f611bb71da5692de01df70a1022c6d5c24f64
Marcel Bollmann <bollmann@linguistics.rub.de>
2015-04-24 15:16:26 +0200

    Added functionality to parse | and # in simple token edit script.

--- bin/cora_edit_singletoken.py
+++ bin/cora_edit_singletoken.py
@@ -5,8 +5,15 @@ import os
 import json
 import argparse
 
+def splitAt(token, symbol):
+    result = token.split(symbol)
+    if len(result) < 2:
+        return result
+    return [x+symbol for x in result[:-1]] + [result[-1]]
+
 class MainApplication(object):
     def __init__(self, args):
+        self.split = args.split
         self.lines = [x.strip() for x in args.infile.readlines()]
         self.token = ' '.join(self.lines)
         args.infile.close()
@@ -16,13 +23,21 @@ class MainApplication(object):
         exit(1)
 
     def performConversions(self):
-        result = {"mod_trans": [self.token],
-                  "mod_ascii": [self.token],
-                  "mod_utf":   [self.token],
-                  "dipl_trans": [self.token],
-                  "dipl_utf":   [self.token],
-                  "dipl_breaks": [0]
-                  }
+        result = {}
+        if self.split:
+            modern = self.token.split('|')
+            result['mod_ascii'] = result['mod_utf'] = \
+                [m.replace('#', '') for m in modern]
+            result['mod_trans'] = [m+'|' for m in modern[:-1]] + [modern[-1]]
+            dipl = self.token.split('#')
+            result['dipl_utf'] = [d.replace('|', '') for d in dipl]
+            result['dipl_trans'] = [d+'#' for d in dipl[:-1]] + [dipl[-1]]
+            result['dipl_breaks'] = [0] * len(dipl)
+        else:
+            result['mod_trans'] = result['mod_ascii'] = result['mod_utf'] = \
+            result['dipl_trans'] = result['dipl_utf'] = [self.token]
+            result['dipl_breaks'] = [0]
+
         return result
 
     def run(self):
@@ -38,6 +53,10 @@ if __name__ == '__main__':
                         nargs='?',
                         type=argparse.FileType('r'),
                         help='Input file')
+    parser.add_argument('-s', '--split',
+                        action='store_true',
+                        default=False,
+                        help='Parse pipe (|) and hash (#) as tokenization symbols')
 #    parser.add_argument('-e', '--encoding',
 #                        default='utf-8',
 #                        help='Encoding of the input file (default: utf-8)')
