0341216dba3232e67dac4e19d653f8a7c2b35bcc
Marcel Bollmann <bollmann@linguistics.rub.de>
2015-05-12 13:24:22 +0200

    Localized PHP login page.

--- gui/login.php
+++ gui/login.php
@@ -2,27 +2,29 @@
 /** @file login.php
  * The login page.
  */
+
+$_ = $lh;
 ?>
 
 <div id="loginDiv" class="content">
 
 <div class="panel">
    <?php if($_SESSION["failedLogin"]): ?>
-   <p style="color: red;">Anmeldung fehlgeschlagen. Bitte überprüfen Sie Ihre Eingaben.</p>
+   <p style="color: red;"><?= $_("Forms.msgFailedLogin") ?></p>
    <?php endif; ?>
 
    <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post" class="loginForm">
      <input type="hidden" name="action" value="login"/>
-      <h4><label for="loginform_un">Benutzername:</label></h4>
+      <h4><label for="loginform_un"><?= $_("Forms.username") ?>:</label></h4>
       <p class="text">
         <input type="text" name="loginform[un]" id="loginform_un" value="" />
       </p>
-   <h4><label for="loginform_pw">Passwort:</label></h4>
+   <h4><label for="loginform_pw"><?= $_("Forms.password") ?>:</label></h4>
       <p class="text">
         <input type="password" name="loginform[pw]" id="loginform_pw" value="" />
       </p>
       <p class="button">
-        <button name="submit_button" value="save" type="submit">Anmelden</button>
+        <button name="submit_button" value="save" type="submit"><?= $_("Forms.buttonLogin") ?></button>
       </p>
    </form>
 
