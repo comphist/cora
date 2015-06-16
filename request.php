8f399f30d5d9706a960b339cca414595bb8d7296
Marcel Bollmann <bollmann@linguistics.rub.de>
2015-06-16 18:09:12 +0200

    JSON requests now send the appropriate MIME type

--- request.php
+++ request.php
@@ -4,10 +4,9 @@ ini_set('memory_limit', '536870912');
 /** @file request.php
  * Handle requests sent via JavaScript.
  *
- * This file is intended to be called from JavaScript code to perform
- * database requests.  It will usually pass them on to a
- * RequestHandler object, which then typically outputs a JSON object
- * string (depending on the nature of the request).
+ * This file is intended to be called from JavaScript code to perform database
+ * requests.  It will usually pass them on to a RequestHandler object, which
+ * then outputs a JSON object string (depending on the nature of the request).
  */
 
 require_once( "lib/globals.php" );
@@ -23,8 +22,10 @@ $exp = new Exporter($dbi);
 $xml = new XMLHandler($dbi);
 $ch = new CommandHandler();
 
-$sh = new CoraSessionHandler($dbi, $xml, $exp, $ch);     /**< An instance of the SessionHandler object. */
-$rq = new RequestHandler($sh);  /**< An instance of the RequestHandler object. */
+$sh = new CoraSessionHandler($dbi, $xml, $exp, $ch);
+$rq = new RequestHandler($sh);
+
+header('Content-Type: application/json');
 
 if($_SESSION["loggedIn"]) {
   $rq->handleJSONRequest( $_GET, $_POST );
