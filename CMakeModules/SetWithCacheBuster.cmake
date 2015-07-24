2de3b2472b32fdceb4a9bce32b04395197c1f78c
Marcel Bollmann <bollmann@linguistics.rub.de>
2015-07-24 18:45:06 +0200

    Generate CSS links with cache buster directly from CMake
    Instead of PHP dynamically generating links to the CSS files with cache
    buster additions, let CMake create them automatically during build
    time.
    
    MD5 hashes based on the files are used for the cache buster string.
    
    Usage of add_sources(...) has been abandoned in favor of direct
    configure_file(...) calls, because this ensures a re-configuration of
    the CMakeLists.txt when one of the source files changes, necessary for
    correctly updating the cache buster string.

---
+++ CMakeModules/SetWithCacheBuster.cmake
@@ -0,0 +1,28 @@
+# This function sets the variable VARNAME (as a global cache variable with
+# description DESC) to a string containing PATH with an appended cache buster
+# string, i.e., a non-functional parameter that should cause the browser to
+# re-request the file iff the contents of HASHFILE have been changed.  Multiple
+# files can be given.
+#
+# The reason for PATH and HASHFILE being separate is as follows: PATH is what
+# the web browser should request, and can potentially refer to a file that
+# hasn't been generated yet at configure time; in these cases, HASHFILE can be
+# used to point to one or more files that actually serve as the basis for
+# generation of PATH.
+#
+# HASHFILE(s) should be relative to CMAKE_CURRENT_SOURCE_DIR.
+
+function(set_with_cache_buster VARNAME DESC PATH HASHFILE)
+  message(STATUS "Generating hash value for ${PATH}")
+
+  file(MD5 "${CMAKE_CURRENT_SOURCE_DIR}/${HASHFILE}" HASH)
+  if(ARGN)
+    set(HASHES "${HASH}")
+    foreach(_HASHFILE ${ARGN})
+      file(MD5 "${CMAKE_CURRENT_SOURCE_DIR}/${_HASHFILE}" CURRENT_HASH)
+      list(APPEND HASHES "${CURRENT_HASH}")
+    endforeach()
+    string(MD5 HASH "${HASHES}")
+  endif()
+  set(${VARNAME} "${PATH}?${HASH}" CACHE STRING "${DESC}" FORCE)
+endfunction()
