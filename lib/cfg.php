dd94993abb8abffbec52200db11f64d997ccf0c2
Marcel Bollmann <bollmann@linguistics.rub.de>
2015-08-12 15:06:25 +0200

    Extend Cfg to set and save user configuration

--- lib/cfg.php
+++ lib/cfg.php
@@ -3,9 +3,9 @@
  * Class to access configuration options
  */
 
-define('CORA_CONFIG_FILE', dirname(__FILE__) . "/../config.php");
+define('CORA_CONFIG_FILE', true);
 
-/** Accessor for configuration options.
+/** Accessor for configuration options
  *
  * Currently, this acts as a simple wrapper around a configuration
  * array defined elsewhere.  By making this a class (instead of accessing
@@ -13,21 +13,52 @@ define('CORA_CONFIG_FILE', dirname(__FILE__) . "/../config.php");
  * implementation/storage of these config variables, though.
  */
 class Cfg {
+  private static $user_config_file;
   private static $user_options;
   private static $default_options;
 
   private static function load_opts() {
-    if (file_exists(CORA_CONFIG_FILE)) {
-      self::$user_options = include CORA_CONFIG_FILE;
-    } else {
-      self::$user_options = array();
-    }
     $options = include dirname(__FILE__) . "/../config.defaults.php";
     $version = include dirname(__FILE__) . "/../version.php";
     self::$default_options = array_merge($options, $version);
+    self::$user_config_file = dirname(__FILE__) . "/../config.php";
+    if (!self::$user_options) { self::load_user_opts(); }
   }
 
-  /** Accesses a configuration variable.
+  /** Load user-defined configuration values
+   *
+   * @param string $filename Name of configuration file to read from;
+   *                         defaults to self::$user_config_file
+   */
+  public static function load_user_opts($filename=null) {
+    if ($filename === null) { $filename = self::$user_config_file; }
+    if (file_exists($filename)) {
+      self::$user_options = include $filename;
+    } else {
+      self::$user_options = array();
+    }
+  }
+
+  /** Save user-defined configuration values
+   *
+   * @param string $filename Name of configuration file to save to;
+   *                         defaults to self::$user_config_file
+   */
+  public static function save_user_opts($filename=null) {
+    if ($filename === null) { $filename = self::$user_config_file; }
+    return file_put_contents(
+      $filename,
+      '<?php return ' . var_export(self::$user_options, true) . ';'
+    );
+  }
+
+  /** Access a configuration variable
+   *
+   * If the variable is set in the user configuration (config.php), it will be
+   * returned from there; otherwise, it is returned from the default
+   * configuration file (config.defaults.php).
+   *
+   * @param string $var Name of the variable
    */
   public static function get($var) {
     if (!self::$default_options) { self::load_opts(); }
@@ -36,6 +67,20 @@ class Cfg {
     }
     return self::$default_options[$var];
   }
+
+  /** Set a configuration variable
+   *
+   * Sets a configuration variable to a given value.  This value will always
+   * be stored in the user configuration.
+   *
+   * @param string $var Name of the variable
+   * @param string $val New value
+   */
+  public static function set($var, $val) {
+    if (!self::$default_options) { self::load_opts(); }
+    self::$user_options[$var] = $val;
+  }
+
 }
 
 ?>
