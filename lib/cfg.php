db3df1ee85ff66b1c8e38d434f946b9dbc3e61e5
Marcel Bollmann <bollmann@linguistics.rub.de>
2015-08-12 13:05:32 +0200

    Add distinction between config.php and config.defaults.php

--- lib/cfg.php
+++ lib/cfg.php
@@ -13,19 +13,28 @@ define('CORA_CONFIG_FILE', dirname(__FILE__) . "/../config.php");
  * implementation/storage of these config variables, though.
  */
 class Cfg {
-  private static $options;
+  private static $user_options;
+  private static $default_options;
 
   private static function load_opts() {
-    $options = include CORA_CONFIG_FILE;
+    if (file_exists(CORA_CONFIG_FILE)) {
+      self::$user_options = include CORA_CONFIG_FILE;
+    } else {
+      self::$user_options = array();
+    }
+    $options = include dirname(__FILE__) . "/../config.defaults.php";
     $version = include dirname(__FILE__) . "/../version.php";
-    self::$options = array_merge($options, $version);
+    self::$default_options = array_merge($options, $version);
   }
 
   /** Accesses a configuration variable.
    */
   public static function get($var) {
-    if (!self::$options) { self::load_opts(); }
-    return self::$options[$var];
+    if (!self::$default_options) { self::load_opts(); }
+    if (array_key_exists($var, self::$user_options)) {
+      return self::$user_options[$var];
+    }
+    return self::$default_options[$var];
   }
 }
 
