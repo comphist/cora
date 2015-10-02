1288a85c19df216630de2f001329bbd2244aca21
Marcel Bollmann <bollmann@linguistics.rub.de>
2015-10-02 17:11:32 +0200

    Add new CLI installation script

---
+++ db/common.php
@@ -0,0 +1,123 @@
+<?php 
+/*
+ * Copyright (C) 2015 Marcel Bollmann <bollmann@linguistics.rub.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */ ?>
+<?php
+require_once __DIR__ . "/../lib/cfg.php";
+require_once __DIR__ . "/../lib/install.php";
+
+$migration_dir = __DIR__ . "/migration";
+
+$default_settings = array(
+  "DBINFO" => array(
+    "HOST" => "127.0.0.1",
+    "USER" => "cora",
+    "PASSWORD" => "trustthetext",
+    "DBNAME" => "cora"
+  ),
+  "DBROOT" => array(
+    "USER" => "root",
+    "PASSWORD" => ""
+  ),
+  "MYSQL_BIN" => "mysql"
+);
+
+function get_settings_from_config() {
+  global $default_settings;
+  $settings = $default_settings;
+  $dbinfo = Cfg::get('dbinfo');
+  if (is_array($dbinfo)) {
+    $settings["DBINFO"] = $dbinfo;
+  }
+  return $settings;
+}
+
+function get_settings_from_options($options) {
+  $settings = get_settings_from_config();
+  foreach($options as $opt => $value) switch($opt) {
+    case 'o':
+    case 'host':
+      $settings["DBINFO"]["HOST"] = $value;
+      break;
+
+    case 'd':
+    case 'database':
+      $settings["DBINFO"]["DBNAME"] = $value;
+      break;
+
+    case 'u':
+    case 'user':
+      $settings["DBINFO"]["USER"] = $value;
+      break;
+
+    case 'p':
+    case 'password':
+      $settings["DBINFO"]["PASSWORD"] = $value;
+      break;
+
+    case 'U':
+    case 'root-user':
+      $settings["DBROOT"]["USER"] = $value;
+      break;
+
+    case 'P':
+    case 'root-password':
+      $settings["DBROOT"]["PASSWORD"] = $value;
+      break;
+
+    case 'b':
+    case 'mysql-bin':
+      $settings["MYSQL_BIN"] = $value;
+      break;
+  }
+
+  $settings["DBROOT"]["HOST"] = $settings["DBINFO"]["HOST"];
+  return $settings;
+}
+
+function make_installer($settings) {
+  $installer = new InstallHelper($settings["DBINFO"]);
+  $installer->mysql_bin = $settings["MYSQL_BIN"];
+  return $installer;
+}
+
+function get_database_status($installer) {
+  $status = array(
+    "can_connect" => $installer->canConnect()
+  );
+  if ($status['can_connect']) {
+    $status['version_current'] = $installer->getDBVersion();
+    $status['version_required'] = Cfg::get("db_version");
+    $status['need_migration'] =
+        ($status['version_current'] != $status['version_required']);
+    if ($status['need_migration']) {
+      global $migration_dir;
+      $status['migration_path'] = $installer->findMigrationPath(
+        $status['version_current'],
+        $status['version_required'],
+        $migration_dir
+      );
+    }
+    $status['can_execute_mysql'] = $installer->canExecuteMySQL();
+  }
+  return $status;
+}
+
+?>
