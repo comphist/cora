5bdf90e1e195fd18d8c961b3a55871ef072cf5f8
Fabian Simonjetz <fabian.simonjetz@rub.de>
2015-11-30 18:39:38 +0100

    localized banners and other strings in the admin tab

--- gui/js/admin.js
+++ gui/js/admin.js
@@ -593,12 +593,12 @@ cora.annotatorEditor = {
                 textDialogOnError: true,
                 onSuccess: function(status) {
                     this.performUpdate();
-                    gui.showNotice('ok', "Tagger gelöscht.");
+                    gui.showNotice('ok', _("Banner.taggerDeleted"));
                 }.bind(this),
                 onComplete: function() { gui.unlock(); }
             }).get({'id': tid});
         }.bind(this);
-        gui.confirm("Tagger "+tid+" '"+name+"' wirklich löschen?",
+        gui.confirm(_("AdminTab.Forms.taggerDelConfirm", {taggerNum: tid, taggerName: name}),
                     performDelete, true);
     },
 
@@ -619,7 +619,7 @@ cora.annotatorEditor = {
                 this.performUpdate(function() {
                     this.showAnnotatorOptionsDialog(status.id);
                 }.bind(this));
-                gui.showNotice('ok', "Tagger erstellt.");
+                gui.showNotice('ok', _("Banner.taggerCreated"));
             }.bind(this),
             onComplete: function() { gui.unlock(); }
         }).post({'name': name, 'class': 'None'});
@@ -664,7 +664,7 @@ cora.annotatorEditor = {
             textDialogOnError: true,
             onSuccess: function(status) {
                 this.performUpdate();
-                gui.showNotice('ok', "Tagger-Optionen geändert.");
+                gui.showNotice('ok', _("Banner.taggerOptionsUpdated"));
             }.bind(this),
             onComplete: function() { gui.unlock(); }
         }).post(annotator);
@@ -729,7 +729,7 @@ cora.annotatorEditor = {
         var performRequest = function(content) {
             var name = content.getElement('input').get('value');
             if(!name || name.length < 1) {
-                gui.showNotice('error', 'Name darf nicht leer sein!');
+                gui.showNotice('error', _("Banner.enterTaggerName"));
                 return false;
             }
             this.createAnnotator(name);
@@ -826,7 +826,7 @@ cora.noticeEditor = {
             textDialogOnError: true,
             onSuccess: function(status) {
                 this.performUpdate();
-                gui.showNotice('ok', "Benachrichtigung hinzugefügt.");
+                gui.showNotice('ok', _("Banner.msgAdded"));
             }.bind(this),
             onComplete: function() { gui.unlock(); }
         }).post({'type': type, 'text': text, 'expires': expires});
@@ -846,12 +846,12 @@ cora.noticeEditor = {
                 textDialogOnError: true,
                 onSuccess: function(status) {
                     this.performUpdate();
-                    gui.showNotice('ok', "Benachrichtigung gelöscht.");
+                    gui.showNotice('ok', _("Banner.msgDeleted"));
                 }.bind(this),
                 onComplete: function() { gui.unlock(); }
             }).get({'id': nid});
         }.bind(this);
-        gui.confirm("Benachrichtigung "+nid+" wirklich löschen?",
+        gui.confirm(_("AdminTab.Forms.msgDelConfirm", {msg: nid}),
                     performDelete, true);
     },
 
@@ -866,11 +866,11 @@ cora.noticeEditor = {
             text = content.getElement('textarea').get('value');
             expires = content.getElement('input').get('value');
             if(!expires || expires.length < 1) {
-                gui.showNotice('error', 'Ablauftermin muss angegeben werden!');
+                gui.showNotice('error', _("Banner.enterExpireDate"));
                 return false;
             }
             if(!text || text.length < 1) {
-                gui.showNotice('error', 'Benachrichtigung darf nicht leer sein!');
+                gui.showNotice('error', _("Banner.enterMsg"));
                 return false;
             }
             this.createNotice(type, text, expires);
@@ -941,7 +941,7 @@ cora.projectEditor = {
                                       }
 			              $('projectCreateForm').getElement('input').set('value', '');
 			              this.close();
-                                      gui.showNotice('ok', 'Projekt angelegt.');
+                                      gui.showNotice('ok', _("Banner.projectCreated"));
                                       cora.projects.performUpdate(function() {
                                           ref.showProjectEditDialog(pid);
                                       });
@@ -998,19 +998,17 @@ cora.projectEditor = {
                     textDialogOnError: true,
                     onSuccess: function(status) {
                         cora.projects.performUpdate();
-                        gui.showNotice('ok', "Projekt gelöscht.");
+                        gui.showNotice('ok', _("Banner.projectDeleted"));
                     }.bind(this),
                     onComplete: function() { gui.unlock(); }
                 }).get({'project_id': pid});
 	    } else {
-                setTimeout(function() {
-                    gui.showMsgDialog('error', 'Projekte können nicht gelöscht werden, '
-                                      + 'solange noch mindestens ein Dokument dem Projekt '
-                                      + 'zugeordnet ist.');
+                setTimeout(function() {p
+                    gui.showMsgDialog('error', _("AdminTab.Forms.delProjectInfo"));
                 }, 10);
 	    }
         };
-        gui.confirm("Projekt "+pid+" '"+pn+"' wirklich löschen?",
+        gui.confirm(_("AdminTab.Forms.deleteProjectConfirm", {projectName: pn, projectId: pid}),
                     performDelete, true);
     },
 
@@ -1247,7 +1245,7 @@ cora.tagsetEditor = {
 	    }
 	    var importfile = $(formname).getElement('input[name="txtFile"]').get('value');
 	    if(importfile==null || importfile=="") {
-                gui.showNotice('error', "Keine Datei zum Importieren ausgewählt!");
+                gui.showNotice('error', _("Banner.noImportFileSelected"));
 		e.stop();
 	    }
 	});
