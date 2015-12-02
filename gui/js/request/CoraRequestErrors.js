c80156519370298d0837d186f7715193a15a98b9
Fabian Simonjetz <fabian.simonjetz@rub.de>
2015-12-03 00:52:37 +0100

    I tried to localize this file, but it appears to cause errors as soon as localization strings are added. E.g. when trying to annotate a text without a valid tagger set, an error message appears ("Aktion konnte nicht ausgeführt werden"). If the string in the message is localized, CorA keeps loading forever without the message popping up.

--- gui/js/request/CoraRequestErrors.js
+++ gui/js/request/CoraRequestErrors.js
@@ -38,7 +38,7 @@ var CoraRequestError = new Class({
            || this.details.length === undefined || this.details.length === 0) {
             this.showAsDialog();
         } else {
-            gui.showTextDialog("Aktion fehlgeschlagen", this.message, this.details);
+            gui.showTextDialog(_("Gui.actionFailed"), this.message, this.details);
         }
     }
 });
@@ -51,7 +51,7 @@ var CoraRequestError = new Class({
 CoraRequestError.Handled = new Class({
     Extends: CoraRequestError,
     name: 'Handled',
-    message: "Die Aktion konnte nicht ausgeführt werden.",
+    message: "Aktion konnte nicht ausgeführt werden.",
     status: {},
     initialize: function(status) {
         this.status  = status;
@@ -68,7 +68,7 @@ CoraRequestError.Handled = new Class({
 CoraRequestError.NotLoggedIn = new Class({
     Extends: CoraRequestError,
     name: 'NotLoggedIn',
-    message: "Sie sind nicht angemeldet."
+    message: _("Gui.notLoggedIn")
 });
 
 /* Class: CoraRequestError.Invalid
