comphist/cora

commit 79cfaa7f2e6d10c29d504796b4d58df9e1c3d801

Move dialog title to PHP template
so it gets properly localized, partially addressing issue #55

gui/js/file.js
@@ -460,7 +460,7 @@ cora.fileImporter = {
         var ref = this;
         var importform = $('newFileImportForm');
         var mbox = new mBox.Modal({
-            title: _("FileTab.Forms.importXmlForm"),
+            title: 'fileImportXMLForm_title',
             content: 'fileImportXMLForm',
             attach: 'importNewXMLLink',
             closeOnBodyClick: false
@@ -496,7 +496,7 @@ cora.fileImporter = {
         var ref = this;
         var importform = $('newFileImportTransForm');
         var mbox = new mBox.Modal({
-            title: _("FileTab.Forms.importTextForm"),
+            title: 'fileImportTransForm_title',
             content: 'fileImportTransForm',
             attach: 'importNewTransLink',
             closeOnBodyClick: false
@@ -556,8 +556,8 @@ cora.fileImporter = {
 	    displayText: true
         });
         this.transImportProgressDialog = new mBox.Modal({
-	    title: _("FileTab.importingData"),
-	    content: $('transImportSpinner'),
+	    title: 'transImportSpinner_title',
+	    content: 'transImportSpinner',
 	    closeOnBodyClick: false,
 	    closeOnEsc: false,
 	    closeInTitle: false,
@@ -1197,11 +1197,6 @@ cora.fileManager = {
          fid - ID of the file to be deleted
      */
     deleteFile: function(fid) {
-        /*var message = "Soll das Dokument '" + cora.files.getDisplayName(fid)
-            + "' wirklich gelöscht werden? Dieser Schritt kann nicht rückgängig "
-            + "gemacht werden!";
-        */
-
         var message = _("FileTab.Forms.deleteWarning",
                         {file: cora.files.getDisplayName(fid)});
         var performDelete = function() {
@@ -1286,12 +1281,11 @@ cora.fileManager = {
          fid - ID of the file to be exported
      */
     exportFile: function(fid){
-        var content = $('fileExportPopup');
         this.exportMultiSelectForCSV(fid)
             .replaces(content.getElement('.export_CustomCSV_MS'));
 	new mBox.Modal({
-	    content: content,
-	    title: _("FileTab.Forms.exportFile"),
+	    content: 'fileExportPopup',
+	    title: 'fileExportPopup_title',
 	    buttons: [
 		{title: _("Action.cancel"), addClass: 'mform'},
 		{title: _("Action.export2"), addClass: 'mform button_green',

gui/file.php
@@ -38,7 +38,7 @@
         <span class="oi" data-glyph="collapse-down" aria-hidden="true"></span>
       </span>
       <span class="btn-toolbar-entry start-disabled" id="importNewTransLink">
-        <span class="oi" data-glyph="data-transfer-upload" aria-hidden="true"></span> 
+        <span class="oi" data-glyph="data-transfer-upload" aria-hidden="true"></span>
         <span data-trans-id="FileTab.importText"><?=$lh("FileTab.importText"); ?></span>
       </span>
       <span class="btn-toolbar-entry start-disabled" id="importNewXMLLink">
@@ -52,6 +52,7 @@
 
   <!-- templates -->
   <div class="templateHolder">
+    <span id="transImportSpinner_title" data-trans-id="FileTab.importingData"><?=$lh("FileTab.importingData"); ?></span>
     <div id="transImportSpinner">
       <div id="transImportStatusContainer">
         <table>
@@ -119,21 +120,22 @@
             <span class="oi oi-shadow oi-adjust" data-glyph="delete" title="<?=$lh("FileTab.deleteFile"); ?>" data-trans-title-id="FileTab.deleteFile" aria-hidden="true"></span>
           </a>
           <a class="exportFileLink">
-            <span class="oi oi-shadow oi-adjust" data-glyph="data-transfer-download" title="<?=$lh("FileTab.exportFileTitle"); ?>" data-trans-title-id="FileTab.exportFileTitle" aria-hidden="true"></span> 
+            <span class="oi oi-shadow oi-adjust" data-glyph="data-transfer-download" title="<?=$lh("FileTab.exportFileTitle"); ?>" data-trans-title-id="FileTab.exportFileTitle" aria-hidden="true"></span>
             <span data-trans-id="Action.export"><?=$lh("Action.export"); ?></span>
           </a>
           <a class="editTagsetAssocLink start-hidden admin-only">
-            <span class="oi oi-shadow oi-adjust" data-glyph="link-intact" title="<?=$lh("FileTab.editTagsetAssociations"); ?>" data-trans-title-id="FileTab.editTagsetAssociations" aria-hidden="true"></span> 
+            <span class="oi oi-shadow oi-adjust" data-glyph="link-intact" title="<?=$lh("FileTab.editTagsetAssociations"); ?>" data-trans-title-id="FileTab.editTagsetAssociations" aria-hidden="true"></span>
             <span data-trans-id="Action.tagsets"><?=$lh("Action.tagsets"); ?>
             </a>
           <a class="closeFileLink start-hidden">
-            <span class="oi oi-shadow" data-glyph="x" title="<?=$lh("FileTab.closeFileTitle"); ?>" data-trans-title-id="FileTab.closeFileTitle" aria-hidden="true"></span> 
+            <span class="oi oi-shadow" data-glyph="x" title="<?=$lh("FileTab.closeFileTitle"); ?>" data-trans-title-id="FileTab.closeFileTitle" aria-hidden="true"></span>
             <span data-trans-id="Action.close"><?=$lh("Action.close"); ?></span>
           </a>
         </td>
       </tr>
     </table>
 
+    <span id="fileExportPopup_title" data-trans-id="FileTab.Forms.exportFile"><?=$lh("FileTab.Forms.exportFile"); ?></span>
     <div id="fileExportPopup" class="limitedWidth">
       <p data-trans-id="FileTab.Forms.exportPrompt"><?=$lh("FileTab.Forms.exportPrompt"); ?></span></p>
       <p class="file-export-format-selector">
@@ -159,6 +161,7 @@
       </span>
     </div>
 
+    <span id="fileImportXMLForm_title" data-trans-id="FileTab.Forms.importXmlForm"><?=$lh("FileTab.Forms.importXmlForm"); ?></span>
     <div id="fileImportXMLForm" class="limitedWidth">
       <form action="request.php" id="newFileImportForm" method="post" accept-charset="utf-8" enctype="multipart/form-data">
       <p class="error_text error_text_import" data-trans-id="FileTab.Forms.chooseFile"><?=$lh("FileTab.Forms.chooseFile"); ?></p>
@@ -193,6 +196,7 @@
       </form>
     </div>
 
+    <span id="fileImportTransForm_title" data-trans-id="FileTab.Forms.importTextForm"><?=$lh("FileTab.Forms.importTextForm"); ?></span>
     <div id="fileImportTransForm" class="limitedWidth">
       <form action="request.php" id="newFileImportTransForm" method="post" accept-charset="utf-8" enctype="multipart/form-data">
       <p class="error_text error_text_import" data-trans-id="FileTab.Forms.chooseFile"><?=$lh("FileTab.Forms.chooseFile"); ?></p>
@@ -230,7 +234,7 @@
         <input type="hidden" name="via" value="iframe" />
       </p>
       <p style="text-align:right;">
-        <input type="submit" value="<?=$lh("FileTab.Forms.importBtn"); ?>" data-trans-value-id="FileTab.Forms.importBtn" /> 
+        <input type="submit" value="<?=$lh("FileTab.Forms.importBtn"); ?>" data-trans-value-id="FileTab.Forms.importBtn" />
       </p>
       </form>
     </div>

commit 046f2e2c3429b848205eb5f3e25c8e422b3fac75

Fix bug with export dialog not opening
introduced in commit 79cfaa7

gui/js/file.js
@@ -1284,7 +1284,7 @@ cora.fileManager = {
      */
     exportFile: function(fid){
         this.exportMultiSelectForCSV(fid)
-            .replaces(content.getElement('.export_CustomCSV_MS'));
+            .replaces($('fileExportPopup').getElement('.export_CustomCSV_MS'));
 	new mBox.Modal({
 	    content: 'fileExportPopup',
 	    title: 'fileExportPopup_title',