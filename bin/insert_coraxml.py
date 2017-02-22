791fb42feac569e0cbbd496641f21077cd6a6756
Marcel Bollmann <bollmann@linguistics.rub.de>
2017-02-22 19:20:35 +0100

    Add copyright notices to Python scripts

--- bin/insert_coraxml.py
+++ bin/insert_coraxml.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Marcel Bollmann, 2013
+
+# Copyright (C) 2013-2017 Marcel Bollmann <bollmann@linguistics.rub.de>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import sys
 from lxml import etree
@@ -47,7 +65,7 @@ class MainApplication:
         if '\t' not in line:
             return (line, '', '')
         return line.split('\t')
-        
+
     def parse_xml(self):
         parser = etree.XMLParser(resolve_entities=True)
         self.xml = etree.parse(self.xmlfile, parser=parser)
@@ -96,8 +114,7 @@ class MainApplication:
                         exit(1)
                 if align:
                     mod.append(etree.Element("norm_align", {"tag": align}))
-                    
-                    
+
     def run(self):
         self.parse_xml()
         self.process_xml()
