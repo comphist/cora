961a00912dc4a5ad1d328ab6d5a3a9d0e405946d
Marcel Bollmann <bollmann@linguistics.rub.de>
2017-07-05 14:54:48 +0200

    Update scripts to include LocaleHandler & use single config file

--- bin/clone_file.php
+++ bin/clone_file.php
@@ -1,6 +1,7 @@
+#!/usr/bin/env php
 <?php 
 /*
- * Copyright (C) 2015 Marcel Bollmann <bollmann@linguistics.rub.de>
+ * Copyright (C) 2015-2017 Marcel Bollmann <bollmann@linguistics.rub.de>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
