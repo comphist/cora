comphist/cora

commit b85190d1fc0a798457037aa235786bac36676a86

Fix locale changing of edit tab dialogs, re issue #55

gui/js/edit/LineJumper.js
@@ -36,12 +36,17 @@ var LineJumper = new Class({
          parent - The parent PageModel, used to perform the jumps
          content - Content of the dialog window
      */
-    initialize: function(parent, content) {
-        var ref = this;
+    initialize: function(parent) {
         this.parent = parent;
         this.parentTable = parent.parent;
+        this._initializeDialog();
+        gui.onLocaleChange(this._initializeDialog.bind(this));
+    },
+
+    _initializeDialog: function() {
+        var ref = this;
         this.mbox = new mBox.Modal({
-	    content: content.clone(),
+	    content: 'jumpToLineForm',
 	    title: _("EditorTab.goToLine"),
 	    buttons: [
 		{title: _("Action.cancel"), addClass: 'mform'},

gui/js/edit/EditorModel.js
@@ -1151,9 +1151,21 @@ var EditorModel = new Class({
     activateAnnotationDialog: function(button) {
 	var ref = this;
 	var mbox;
-	var content = $('automaticAnnotationForm');
+        this._initializeAnnotationDialog();
+        button.addEvent('click', function() { ref.mboxAnnotation.open(); });
+        gui.onLocaleChange(function() {
+            this._initializeAnnotationDialog();
+            this.prepareAnnotationOptions();
+        }.bind(this));
+    },
+
+    _initializeAnnotationDialog: function() {
+        var ref = this;
+	var content = 'automaticAnnotationForm';
 	var performAnnotation = function(action) {
-	    var taggerID = $('automaticAnnotationForm').getElement('input[name="aa_tagger_select"]:checked').get('value');
+	    var taggerID = $(content)
+                    .getElement('input[name="aa_tagger_select"]:checked')
+                    .get('value');
             new CoraRequest({
                 name: 'performAnnotation',
                 textDialogOnError: true,
@@ -1172,14 +1184,13 @@ var EditorModel = new Class({
 		    gui.hideSpinner();
                 }
             }).get({'action': action, 'tagger': taggerID});
-	    mbox.close();
+	    ref.mboxAnnotation.close();
 	};
 
 	// define the dialog window
-	mbox = new mBox.Modal({
+	ref.mboxAnnotation = new mBox.Modal({
 	    title: _("EditorTab.autoAnnotationTitle"),
-	    content: 'automaticAnnotationForm',
-	    attach: button,
+	    content: content,
             onOpen: function() { ref.save(); },
 	    buttons: [ {title: _("Action.retrain"), addClass: "mform button_left button_yellow",
                         id: "trainStartButton",

gui/js/edit/PageModel.js
@@ -29,7 +29,6 @@
 var PageModel = new Class({
     parent: null,
     lineJumper: null,
-    lineJumperForm: 'jumpToLineForm',
     panels: [],
     maxPage: 0,
     activePage: 0,
@@ -39,7 +38,7 @@ var PageModel = new Class({
         this.buttonsPageBack = new Elements();
         this.buttonsPageForward = new Elements();
         this._calculateMaxPage();
-        this.lineJumper = new LineJumper(this, $(this.lineJumperForm));
+        this.lineJumper = new LineJumper(this);
     },
 
     /* Function: _calculateMaxPage

gui/js/edit/TokenSearcher.js
@@ -57,43 +57,13 @@ var TokenSearcher = new Class({
     templateListElem: null,
 
     initialize: function(parent, tagsets, flags, options) {
-        var content, ref = this;
         this.setOptions(options);
         this.parent = parent;
         this.tagsets = tagsets;
         this.flagHandler = flags;
         this.templateListElem = $(this.options.template);
-
-        this._initializeTemplate();
-        content = $(this.options.content);
-        this.flexrow = new FlexRowList(content.getElement('.flexrow-container'),
-                                       this.templateListElem);
-        this.reset();
-        this.mbox = new mBox.Modal({
-	    content: content,
-	    title: _("Action.search"),
-            closeOnBodyClick: false,
-	    buttons: [
-                {title: _("Action.reset"), addClass: 'mform button_left',
-                 event: function() { ref.reset(); }
-                },
-		{title: _("Action.cancel"), addClass: 'mform'},
-		{title: _("Action.search"), addClass: 'mform button_green',
-		 event: function() {
-                     var spinner = new Spinner(this.mbox.container);
-                     spinner.show();
-                     this.parent.whenSaved(
-                         function() {
-                             this.requestSearch(this.mbox, spinner);
-                         }.bind(this),
-                         null,
-                         function() { spinner.hide(); }
-                     );
-                 }.bind(this)
-		}
-	    ]
-	});
-        this._initializeEvents();
+        this._initializeDialog();
+        gui.onLocaleChange(this._initializeDialog.bind(this));
         if(this.options.panels) {
             Array.each(this.options.panels, this._activateSearch.bind(this));
         }
@@ -113,6 +83,43 @@ var TokenSearcher = new Class({
         }
     },
 
+    /* Function: _initializeDialog
+
+       Initializes the dialog window for initiating a search.
+     */
+    _initializeDialog: function() {
+        var content = $(this.options.content);
+        this._initializeTemplate();
+        this.flexrow = new FlexRowList(content.getElement('.flexrow-container'),
+                                       this.templateListElem);
+        this.reset();
+        this.mbox = new mBox.Modal({
+	    content: content,
+	    title: _("Action.search"),
+            closeOnBodyClick: false,
+	    buttons: [
+                {title: _("Action.reset"), addClass: 'mform button_left',
+                 event: function() { this.reset(); }.bind(this)
+                },
+		{title: _("Action.cancel"), addClass: 'mform'},
+		{title: _("Action.search"), addClass: 'mform button_green',
+		 event: function() {
+                     var spinner = new Spinner(this.mbox.container);
+                     spinner.show();
+                     this.parent.whenSaved(
+                         function() {
+                             this.requestSearch(this.mbox, spinner);
+                         }.bind(this),
+                         null,
+                         function() { spinner.hide(); }
+                     );
+                 }.bind(this)
+		}
+	    ]
+	});
+        this._initializeEvents();
+    },
+
     _initializeTemplate: function() {
         this._initializeFieldSelector();
     },
@@ -127,9 +134,9 @@ var TokenSearcher = new Class({
         fieldSelector.empty();
         // Fixed elements
         fieldSelector.grab(makeOption('token_all',
-                                      _("EditorTab.dropDown.tokenAll"))); // cora.strings.search_condition.field['token_all']
+                                      _("EditorTab.dropDown.tokenAll")));
         fieldSelector.grab(makeOption('token_trans',
-                                     _("EditorTab.dropDown.tokenTrans"))); // cora.strings.search_condition.field['token_trans']
+                                     _("EditorTab.dropDown.tokenTrans")));
         // Annotation layers
         optgroup = new Element('optgroup', {'label': _("EditorTab.dropDown.annotationLevels")});
         Object.each(this.tagsets, function(tagset) {
@@ -168,15 +175,6 @@ var TokenSearcher = new Class({
                 }
             }.bind(this)
         );
-        // silly agreement hack ...
-        this.mbox.content.addEvent(
-            'change:relay(select.editSearchOperator)',
-            function(event, target) {
-                var span = target.getParent('p').getElement('span.eso-det-agr');
-                var selected = target.getSelected()[0].get('value');
-                span.set('text', (selected === "any") ? 'dieser' : 'diese');
-            }
-        );
     },
 
     /* Function: _makeSearchOptions
@@ -186,22 +184,22 @@ var TokenSearcher = new Class({
      */
     _makeSearchOptions: function(cls) {
         var makeOption = function(a,b) {
-            return new Element('option', {'value': a, 'text': b});
+            return new Element('option', {'value': a, 'text': _(b), 'data-trans-id': b});
         };
         if(cls === 'flags') {
             return new Elements([
-                makeOption('set', _("EditorTab.dropDown.isSet")), // cora.strings.search_condition.match['set']
-                makeOption('nset', _("EditorTab.dropDown.isNotSet")) // cora.strings.search_condition.match['nset']
+                makeOption('set', "EditorTab.dropDown.isSet"),
+                makeOption('nset', "EditorTab.dropDown.isNotSet")
             ]);
         }
         return new Elements([
-            makeOption('eq', _("EditorTab.dropDown.is")), // cora.strings.search_condition.match['eq']
-            makeOption('neq', _("EditorTab.dropDown.isNot")), // cora.strings.search_condition.match['neq']
-            makeOption('in', _("EditorTab.dropDown.contains")), // cora.strings.search_condition.match['in']
-            makeOption('nin', _("EditorTab.dropDown.containsNot")), // cora.strings.search_condition.match['nin']
-            makeOption('bgn', _("EditorTab.dropDown.startsWith")), // cora.strings.search_condition.match['bgn']
-            makeOption('end', _("EditorTab.dropDown.endsWith")), // cora.strings.search_condition.match['end']
-            makeOption('regex', _("EditorTab.dropDown.matchesRegEx")) // cora.strings.search_condition.match['regex']
+            makeOption('eq', "EditorTab.dropDown.is"),
+            makeOption('neq', "EditorTab.dropDown.isNot"),
+            makeOption('in', "EditorTab.dropDown.contains"),
+            makeOption('nin', "EditorTab.dropDown.containsNot"),
+            makeOption('bgn', "EditorTab.dropDown.startsWith"),
+            makeOption('end', "EditorTab.dropDown.endsWith"),
+            makeOption('regex', "EditorTab.dropDown.matchesRegEx")
         ]);
     },
 

commit 2719ca220643f30e1e7bc80b498f77f05b3046aa

Fix LineJumper with multiple instances (#99)
Fixes a bug with the line jumper dialog box that was probably introduced in b85190d; namely, when the line jumper class is re-initialized, its contents are moved over to the new instance, leaving the old instance content-less.

This happens, for example, when starting a search or changing the language.

src/gui/js/edit/LineJumper.js
@@ -46,7 +46,7 @@ var LineJumper = new Class({
     _initializeDialog: function() {
         var ref = this;
         this.mbox = new mBox.Modal({
-	    content: 'jumpToLineForm',
+	    content: $('jumpToLineForm').clone(),
 	    title: _("EditorTab.topButton.goToLine"),
 	    buttons: [
 		{title: _("Action.cancel"), addClass: 'mform'},

tests/frontend/CMakeLists.txt
@@ -32,12 +32,17 @@ file(RELATIVE_PATH CORA_TESTS_HREF_MOOTOOLS_CORE
 file(RELATIVE_PATH CORA_TESTS_HREF_MOOTOOLS_MORE
   "${CMAKE_CURRENT_BINARY_DIR}"
   "${CMAKE_SOURCE_DIR}/src/${CORA_HREF_JS_MOOTOOLS_MORE}")
+file(RELATIVE_PATH CORA_TESTS_HREF_VENDOR_DIR
+  "${CMAKE_CURRENT_BINARY_DIR}"
+  "${CMAKE_SOURCE_DIR}/src/gui/js/vendor")
 
 set(FRONTEND_TESTS
   test_tagsets.html
+  test_LineJumper.html
   )
 set(FRONTEND_TEST_ASSETS
   test_tagsets.js
+  test_LineJumper.js
   )
 
 configure_all("${CMAKE_CURRENT_BINARY_DIR}" ${FRONTEND_TESTS})

tests/frontend/test_LineJumper.html.in
@@ -1,0 +1,36 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>CorA Frontend Tests</title>
+        <link rel="stylesheet" href="../../node_modules/mocha/mocha.css" />
+    </head>
+    <body>
+        <div id="mocha"></div>
+        <script src="../../node_modules/mocha/mocha.js"></script>
+        <script src="../../node_modules/chai/chai.js"></script>
+        <script>
+            mocha.ui('bdd');
+            mocha.reporter('html');
+            var expect = chai.expect;
+
+            /* Global variables expected by CorA */
+            var cora = {
+                strings: {}
+            };
+        </script>
+        <script src="@CORA_TESTS_HREF_MOOTOOLS_CORE@"></script>
+        <script src="@CORA_TESTS_HREF_MOOTOOLS_MORE@"></script>
+        <script src="@CORA_TESTS_HREF_VENDOR_DIR@/mbox/mBox.Core.js"></script>
+        <script src="@CORA_TESTS_HREF_VENDOR_DIR@/mbox/mBox.Modal.js"></script>
+        <script src="@CORA_TESTS_JS_DIR@/gui.js"></script>
+        <script src="@CORA_TESTS_JS_DIR@/edit/LineJumper.js"></script>
+        <script src="./test_LineJumper.js"></script>
+        <div id="jumpToLineForm">
+          <label for="jumpTo">Jump to:</label>
+          <input id="jumpToBox" type="text" name="jumpTo" placeholder="" size="6" class="mform" />
+        </div>
+        <script>
+            mocha.run();
+        </script>
+    </body>
+</html>

tests/frontend/test_LineJumper.js
@@ -1,0 +1,32 @@
+describe('LineJumper', function() {
+    var obj;
+
+    before(function() {
+        obj = new LineJumper({parent: null});
+    });
+
+    describe('#mbox', function() {
+        it('should be instantiated', function() {
+            expect(obj.mbox).to.be.not.null;
+        });
+
+        it('should contain the proper form', function() {
+            var content = obj.mbox.content;
+            expect(content).to.be.not.empty;
+            expect(content.getElement('input[name="jumpTo"]')).to.be.not.empty;
+        });
+    });
+
+    describe('#open', function() {
+        it('should not throw exceptions', function() {
+            obj.open();
+        });
+    });
+
+    it('should work with multiple instances', function() {
+        var obj1 = new LineJumper({parent: null});
+        var obj2 = new LineJumper({parent: null});
+        expect(obj1.mbox.content.getElement('input[name="jumpTo"]')).to.be.not.empty;
+        expect(obj2.mbox.content.getElement('input[name="jumpTo"]')).to.be.not.empty;
+    });
+});